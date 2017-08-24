0f626e7cbe9e476f4689d6ea98567140937b9915
PheiPheiPhei <philukas@gmail.com>
2017-08-24 19:03:04 +0200

    Added wireRequestHandler, trying to send position over Wire

--- firmware/stepper_nano_zero/stepper_nano_zero.ino
+++ firmware/stepper_nano_zero/stepper_nano_zero.ino
@@ -1,13 +1,28 @@
 #include "nzs.h"
+#include "stepper_controller.h"
+#include <Wire.h>
 
 NZS nzs;
   
 
+extern StepperCtrl stepperCtrl;
 void setup() {
   nzs.begin();
+  Wire.begin(0x08);
+  Wire.onRequest(wireRequestHandler);
 }
 
 
 void loop() {
 	nzs.loop(); 
 }
+
+void wireRequestHandler() {
+  union {
+    byte b[4];
+    float fval;
+  } u;
+  u.fval = ANGLE_T0_DEGREES(ptrsCtrl.getCurrentAngle());
+  Wire.write(u.b, 4);
+}
+
